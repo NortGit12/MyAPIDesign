40f75f22ff1d698f0e395eab66d4d84f849e316e
Jeff Norton <jeff12ntn@gmail.com>
2016-07-14 11:33:04 -0600

    Implemented put method in SurveyController

--- MyAPIDesign/SurveyController.swift
+++ MyAPIDesign/SurveyController.swift
@@ -16,6 +16,56 @@ class SurveyController {
     
     static let getterEndpoint = baseURL?.URLByAppendingPathExtension("json")
     
+    weak var delegate: SurveyDelegate?
+    
+    var surveys: [Survey] {
+        
+        didSet{
+            delegate.surveysUpdated(self)
+        }
+        
+    }
+    
     // MARK: - Initializer(s)
     
+    init() {
+        
+        self.surveys = []
+        
+        // TODO: fetch call
+        
+    }
+    
+    // MARK: - Methods
+    
+    func putSurveyIntoAPI(name: String, response: String) {
+        
+        let survey = Survey(name: name, response: response)
+        
+        guard let unwrappedURL = survey.endpoint else { return }
+        
+        NetworkController.performRequestForURL(unwrappedURL, httpMethod: .Put, body: survey.jsonData) { (data, error) in
+            
+            let responseDataString = NSString(data: data!, encoding: NSUTF8StringEncoding) ?? ""
+            
+            if error != nil {
+                
+                print("Error (in instance): \(error?.localizedDescription)")
+                
+            } else if responseDataString.containsString("error") {
+                
+                print("Error (in message): \(responseDataString)")
+                
+            } else {
+                
+                print("Saved survey successfully")
+                
+            }
+        }
+    }
+    
+}
+
+protocol SurveyDelegate: class {
+    func surveysUpdated(surveys: [Survey])
 }
