41a73d2bfd35109463df81b5d770ec1f4a86d0c9
Jeff Norton <jeff12ntn@gmail.com>
2016-07-14 11:05:52 -0600

    Added model and its controller

---
+++ MyAPIDesign/Survey.swift
@@ -0,0 +1,9 @@
+//
+//  Survey.swift
+//  MyAPIDesign
+//
+//  Created by Jeff Norton on 7/14/16.
+//  Copyright © 2016 JCN. All rights reserved.
+//
+
+import Foundation
---
+++ MyAPIDesign/SurveyController.swift
@@ -0,0 +1,9 @@
+//
+//  SurveyController.swift
+//  MyAPIDesign
+//
+//  Created by Jeff Norton on 7/14/16.
+//  Copyright © 2016 JCN. All rights reserved.
+//
+
+import Foundation
